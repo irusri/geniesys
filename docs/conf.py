0bcd92fb8c7ac3b00c5454dbb7cf3a000660fb12
Chanaka Mannapperuma <irusri@yahoo.com>
2017-02-20 20:10:59 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,13 @@
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.doctest']
 
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
