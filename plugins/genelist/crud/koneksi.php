0fbb8754e352d895e48c0cdcde874d2973fc39e1
Chanaka Mannapperuma <irusri@yahoo.com>
2017-02-27 08:40:39 +0100

    Update koneksi.php

--- plugins/genelist/crud/koneksi.php
+++ plugins/genelist/crud/koneksi.php
@@ -3,6 +3,7 @@ $path = $_SERVER['DOCUMENT_ROOT'];
 $path .= "/plugins/settings.php";
 include_once($path);
 global $db_url;
+echo $db_ur;
 $private_url = parse_url($db_url['genelist']);
 mysql_connect($db_url, $private_url['user'], $private_url['pass']) or die(mysql_error());
 mysql_select_db(str_replace('/', '', $private_url['path'])) or die(mysql_error());
