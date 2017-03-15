957d55b0ddae27a612ac1f2c018fa680ca8c3e9d
Chanaka Mannapperuma <irusri@yahoo.com>
2017-03-15 20:55:59 +0100

    migrate from mysql to mysqli

--- plugins/genelist/crud/js/init_form.js
+++ plugins/genelist/crud/js/init_form.js
@@ -1,33 +1,29 @@
-$(function(){
-						
-
-	
-	$("#formBarang").submit(function(){
-		if(document.getElementById('namebarangid').value!=""){
-		$.ajax({
-			url:$(this).attr("action"),
-			type:$(this).attr("method"),
-			data:$(this).serialize(),
-			success:function(data){
-				if(data==1)
-				{
-					$("#content").load("plugins/genelist/crud/listbarang.php");
-					page=$(this).attr("href")
-					$("#Formcontent").html("").unload(page);
-					$("#cancelbtn").hide();
-					return false
+//Initializing from crud/formbarang
+$(function() {
+	$("#formBarang").submit(function() {
+		if (document.getElementById('namebarangid').value != "") {
+			$.ajax({
+				url: $(this).attr("action"),
+				type: $(this).attr("method"),
+				data: $(this).serialize(),
+				success: function(data) {
+					if (data == 1)
+					{
+						$("#content").load("plugins/genelist/crud/listbarang.php");
+						page = $(this).attr("href")
+						$("#Formcontent").html("").unload(page);
+						$("#cancelbtn").hide();
+						return false
+					}
+					else
+					{
+						alert(data);
+					}
 				}
-				else
-				{
-					alert(data);
-				}
-			}
-		});
-		}else{
-		alert('Please type in name to new gene list!');	
+			});
+		} else {
+			alert('Please type in name to new gene list!');
 		}
-		
 		return false;
 	});
-	
 })
