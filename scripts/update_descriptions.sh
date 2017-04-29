e6c307b11e76b35142424f45303f6c6c87940844
Chanaka Mannapperuma <irusri@yahoo.com>
2017-04-29 18:35:40 +0200

    Update update_descriptions.sh

--- scripts/update_descriptions.sh
+++ scripts/update_descriptions.sh
@@ -12,7 +12,7 @@ DB='database_name'
                 start_0='\033[0;33m'
                 start_2='\033[0;31m'
                 end='\033[0m'
-                echo  "\nUsage:\n$0 ${start}[gene_info/transcript_info] [file_name]${end}\nEx: ${start_2}sh update_descriptions.sh transcript_info/gene_info potra_descriptions.tsv${end}\n\nWhat it does?\n${start_0}The transcript_ids/gene_ids and update the gene/transcript descriptions.\nFinally delete the temporary table.\n${end}"
+                echo  "\nUsage:\n$0 ${start}[gene_info/transcript_info] [file_name]${end}\nEx: ${start_2}sh update_descriptions.sh transcript_info/gene_info potra_descriptions.tsv${end}\n\nWhat it does?\n${start_0}This script will create a two columns(ids, descriptions) temporary table and load the [file_name] into it.\nThen it will match ids column in temporary table with transcript_ids/gene_ids and update the gene/transcript descriptions.\nFinally delete the temporary table.\n${end}"
                 exit 1
         fi
 
