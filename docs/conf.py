80918876cc62a36fcc51c3a1ff7987fab4bc87d8
Chanaka Mannapperuma <irusri@yahoo.com>
2017-05-01 08:20:14 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -52,8 +52,8 @@ source_suffix = ['.rst', '.md']
 master_doc = 'index'
 
 # General information about the project.
-project = u'GenIECMS'
-copyright = u'2017, GenIECMS'
+project = u'GenIE-CMS'
+copyright = u'2017, GenIE-CMS'
 author = u'Chanaka Mannapperuma'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -114,7 +114,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'GenIECMSdoc'
+htmlhelp_basename = 'GenIE-CMSdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -141,7 +141,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'GenIECMS.tex', u'GenIECMS Documentation',
+    (master_doc, 'GenIE-CMS.tex', u'GenIE-CMS Documentation',
      u'Chanaka Mannapperuma', 'manual'),
 ]
 
@@ -151,7 +151,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'geniecms', u'GenIECMS Documentation',
+    (master_doc, 'geniecms', u'GenIE-CMS Documentation',
      [author], 1)
 ]
 
@@ -162,8 +162,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'GenIECMS', u'GenIECMS Documentation',
-     author, 'GenIECMS', 'One line description of project.',
+    (master_doc, 'GenIE-CMS', u'GenIE-CMS Documentation',
+     author, 'GenIE-CMS', 'One line description of project.',
      'Miscellaneous'),
 ]
 
