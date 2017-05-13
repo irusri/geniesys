8e7ebefcb613c91de74fe16bf161b545d6812fec
Chanaka Mannapperuma <irusri@yahoo.com>
2017-05-13 10:19:47 +0200

    Update load_data.sh

--- scripts/load_data.sh
+++ scripts/load_data.sh
@@ -3,11 +3,10 @@
 #USAGE: sh load_data.sh [table_name] [filename]
 #sh load_data.sh transcript_info_x /tmp/transcript_info.tsv
 
-DB_USER='your_db_username'
-DB_PASS='your_password'
-DB='database_name'
+#Include settings file
+. "$(dirname "$0")"/settings.ini
 
-/usr/bin/mysql --host=localhost --user=$DB_USER --password=$DB_PASS --local_infile=1 --database=$DB <<EOFMYSQL
+/usr/bin/mysql --host=${HOST} -u ${DB_USER} -p${DB_PASS} --local_infile=1 --database=${DB}<<EOFMYSQL
 TRUNCATE TABLE $1;
 ALTER TABLE $1 AUTO_INCREMENT = 1;
 load data local infile '$2' replace INTO TABLE $1 fields terminated by '\t' LINES TERMINATED BY '\n' ignore 0 lines;
