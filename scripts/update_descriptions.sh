00503a4ccc6ab99e4df0c8d53b7289f00e37b551
Chanaka Mannapperuma <irusri@yahoo.com>
2017-05-13 10:22:55 +0200

    Update update_descriptions.sh

--- scripts/update_descriptions.sh
+++ scripts/update_descriptions.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #update_descriptions.sh
 
-DB_USER='your_db_username'
-DB_PASS='your_password'
-DB='database_name'
+#Include settings file
+. "$(dirname "$0")"/settings.ini
 
 # if less than two arguments supplied, display  error message
         if [  $# -le 0 ]
@@ -18,7 +17,7 @@ DB='database_name'
 
 table_name=$(echo $1 | awk '{split($0,a,"_");print a[1]}');
 tmp_field_name=$table_name"_id"
-/usr/bin/mysql --host=localhost --user=$DB_USER --password=$DB_PASS --local_infile=1 --database=$DB<<EOFMYSQL
+/usr/bin/mysql --host=${HOST} -u ${DB_USER} -p${DB_PASS} --local_infile=1 --database=${DB}<<EOFMYSQL
 CREATE TEMPORARY TABLE tmp_tb(gene_name VARCHAR(60),annotation VARCHAR(1000));
 load data local infile '$2' replace INTO TABLE tmp_tb fields terminated by '\t' LINES TERMINATED BY '\n' ignore 0 lines;
 UPDATE $1 INNER JOIN tmp_tb on tmp_tb.gene_name = $1.$tmp_field_name SET $1.description = tmp_tb.annotation;
