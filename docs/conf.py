c12fa1540c1b9a28ad1b84464ce664f5b59f26fe
irusri@yahoo.com <irusri@yahoo.com>
2019-05-30 18:53:52 +0200

    allow database with data

--- docs/conf.py
+++ docs/conf.py
@@ -89,7 +89,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx-bootstrap-theme'
 #html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
