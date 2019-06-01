c1b486ebcc018b222b3bcb1c834080a8f25cb0f8
irusri@yahoo.com <irusri@yahoo.com>
2019-06-01 19:22:38 +0200

    changing document root

--- js/print.js
+++ js/print.js
@@ -1,16 +1,20 @@
-;(function (name, context, definition) {
-  if (typeof module !== 'undefined' && module.exports) { module.exports = definition(); }
-  else if (typeof define === 'function' && define.amd) { define(definition); }
-  else { context[name] = definition(); }
-})('Fingerprint', this, function () {
-  'use strict';
+(function(name, context, definition) {
+  if (typeof module !== "undefined" && module.exports) {
+    module.exports = definition();
+  } else if (typeof define === "function" && define.amd) {
+    define(definition);
+  } else {
+    context[name] = definition();
+  }
+})("Fingerprint", this, function() {
+  "use strict";
 
-  var Fingerprint = function (options) {
+  var Fingerprint = function(options) {
     var nativeForEach, nativeMap;
     nativeForEach = Array.prototype.forEach;
     nativeMap = Array.prototype.map;
 
-    this.each = function (obj, iterator, context) {
+    this.each = function(obj, iterator, context) {
       if (obj === null) {
         return;
       }
@@ -41,30 +45,31 @@
       return results;
     };
 
-    if (typeof options == 'object'){
+    if (typeof options == "object") {
       this.hasher = options.hasher;
       this.screen_resolution = options.screen_resolution;
       this.canvas = options.canvas;
       this.ie_activex = options.ie_activex;
-    } else if(typeof options == 'function'){
+    } else if (typeof options == "function") {
       this.hasher = options;
     }
   };
 
   Fingerprint.prototype = {
-    get: function(){
+    get: function() {
       var keys = [];
       keys.push(navigator.userAgent);
       keys.push(navigator.language);
       keys.push(screen.colorDepth);
-	   /*if (ip_address!="") {
+      /*if (ip_address!="") {
 		 keys.push(ip_address);
 	   }*/
 
       if (this.screen_resolution) {
         var resolution = this.getScreenResolution();
-        if (typeof resolution !== 'undefined'){ // headless browsers, such as phantomjs
-          keys.push(this.getScreenResolution().join('x'));
+        if (typeof resolution !== "undefined") {
+          // headless browsers, such as phantomjs
+          keys.push(this.getScreenResolution().join("x"));
         }
       }
       keys.push(new Date().getTimezoneOffset());
@@ -72,25 +77,23 @@
       keys.push(this.hasLocalStorage());
       keys.push(!!window.indexedDB);
       //body might not be defined at this point or removed programmatically
-      if(document.body){
-        keys.push(typeof(document.body.addBehavior));
+      if (document.body) {
+        keys.push(typeof document.body.addBehavior);
       } else {
         keys.push(typeof undefined);
       }
-      keys.push(typeof(window.openDatabase));
+      keys.push(typeof window.openDatabase);
       keys.push(navigator.cpuClass);
       keys.push(navigator.platform);
       keys.push(navigator.doNotTrack);
       keys.push(this.getPluginsString());
-      if(this.canvas && this.isCanvasSupported()){
+      if (this.canvas && this.isCanvasSupported()) {
         keys.push(this.getCanvasFingerprint());
       }
-      if(this.hasher){
-        return this.hasher(keys.join('###'), 31);
+      if (this.hasher) {
+        return this.hasher(keys.join("###"), 31);
       } else {
-
-        return this.murmurhash3_32_gc(keys.join('###'), 31);
-
+        return this.murmurhash3_32_gc(keys.join("###"), 31);
       }
     },
 
@@ -108,7 +111,6 @@
      */
 
     murmurhash3_32_gc: function(key, seed) {
-
       var remainder, bytes, h1, h1b, c1, c2, k1, i;
 
       remainder = key.length & 3; // key.length % 4
@@ -118,141 +120,171 @@
       c2 = 0x1b873593;
       i = 0;
       while (i < bytes) {
-          k1 =
-            ((key.charCodeAt(i) & 0xff)) |
-            ((key.charCodeAt(++i) & 0xff) << 8) |
-            ((key.charCodeAt(++i) & 0xff) << 16) |
-            ((key.charCodeAt(++i) & 0xff) << 24);
+        k1 =
+          (key.charCodeAt(i) & 0xff) |
+          ((key.charCodeAt(++i) & 0xff) << 8) |
+          ((key.charCodeAt(++i) & 0xff) << 16) |
+          ((key.charCodeAt(++i) & 0xff) << 24);
 
         ++i;
 
-        k1 = ((((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16))) & 0xffffffff;
+        k1 =
+          ((k1 & 0xffff) * c1 + ((((k1 >>> 16) * c1) & 0xffff) << 16)) &
+          0xffffffff;
         k1 = (k1 << 15) | (k1 >>> 17);
-        k1 = ((((k1 & 0xffff) * c2) + ((((k1 >>> 16) * c2) & 0xffff) << 16))) & 0xffffffff;
+        k1 =
+          ((k1 & 0xffff) * c2 + ((((k1 >>> 16) * c2) & 0xffff) << 16)) &
+          0xffffffff;
 
         h1 ^= k1;
-            h1 = (h1 << 13) | (h1 >>> 19);
-        h1b = ((((h1 & 0xffff) * 5) + ((((h1 >>> 16) * 5) & 0xffff) << 16))) & 0xffffffff;
-        h1 = (((h1b & 0xffff) + 0x6b64) + ((((h1b >>> 16) + 0xe654) & 0xffff) << 16));
+        h1 = (h1 << 13) | (h1 >>> 19);
+        h1b =
+          ((h1 & 0xffff) * 5 + ((((h1 >>> 16) * 5) & 0xffff) << 16)) &
+          0xffffffff;
+        h1 =
+          (h1b & 0xffff) + 0x6b64 + ((((h1b >>> 16) + 0xe654) & 0xffff) << 16);
       }
 
       k1 = 0;
 
       switch (remainder) {
-        case 3: k1 ^= (key.charCodeAt(i + 2) & 0xff) << 16;
-        case 2: k1 ^= (key.charCodeAt(i + 1) & 0xff) << 8;
-        case 1: k1 ^= (key.charCodeAt(i) & 0xff);
+        case 3:
+          k1 ^= (key.charCodeAt(i + 2) & 0xff) << 16;
+        case 2:
+          k1 ^= (key.charCodeAt(i + 1) & 0xff) << 8;
+        case 1:
+          k1 ^= key.charCodeAt(i) & 0xff;
 
-        k1 = (((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16)) & 0xffffffff;
-        k1 = (k1 << 15) | (k1 >>> 17);
-        k1 = (((k1 & 0xffff) * c2) + ((((k1 >>> 16) * c2) & 0xffff) << 16)) & 0xffffffff;
-        h1 ^= k1;
+          k1 =
+            ((k1 & 0xffff) * c1 + ((((k1 >>> 16) * c1) & 0xffff) << 16)) &
+            0xffffffff;
+          k1 = (k1 << 15) | (k1 >>> 17);
+          k1 =
+            ((k1 & 0xffff) * c2 + ((((k1 >>> 16) * c2) & 0xffff) << 16)) &
+            0xffffffff;
+          h1 ^= k1;
       }
 
       h1 ^= key.length;
 
       h1 ^= h1 >>> 16;
-      h1 = (((h1 & 0xffff) * 0x85ebca6b) + ((((h1 >>> 16) * 0x85ebca6b) & 0xffff) << 16)) & 0xffffffff;
+      h1 =
+        ((h1 & 0xffff) * 0x85ebca6b +
+          ((((h1 >>> 16) * 0x85ebca6b) & 0xffff) << 16)) &
+        0xffffffff;
       h1 ^= h1 >>> 13;
-      h1 = ((((h1 & 0xffff) * 0xc2b2ae35) + ((((h1 >>> 16) * 0xc2b2ae35) & 0xffff) << 16))) & 0xffffffff;
+      h1 =
+        ((h1 & 0xffff) * 0xc2b2ae35 +
+          ((((h1 >>> 16) * 0xc2b2ae35) & 0xffff) << 16)) &
+        0xffffffff;
       h1 ^= h1 >>> 16;
 
       return h1 >>> 0;
     },
 
     // https://bugzilla.mozilla.org/show_bug.cgi?id=781447
-    hasLocalStorage: function () {
-      try{
+    hasLocalStorage: function() {
+      try {
         return !!window.localStorage;
-      } catch(e) {
+      } catch (e) {
         return true; // SecurityError when referencing it means it exists
       }
     },
 
-    hasSessionStorage: function () {
-      try{
+    hasSessionStorage: function() {
+      try {
         return !!window.sessionStorage;
-      } catch(e) {
+      } catch (e) {
         return true; // SecurityError when referencing it means it exists
       }
     },
 
-    isCanvasSupported: function () {
-      var elem = document.createElement('canvas');
-      return !!(elem.getContext && elem.getContext('2d'));
+    isCanvasSupported: function() {
+      var elem = document.createElement("canvas");
+      return !!(elem.getContext && elem.getContext("2d"));
     },
 
-    isIE: function () {
-      if(navigator.appName === 'Microsoft Internet Explorer') {
+    isIE: function() {
+      if (navigator.appName === "Microsoft Internet Explorer") {
         return true;
-      } else if(navigator.appName === 'Netscape' && /Trident/.test(navigator.userAgent)){// IE 11
+      } else if (
+        navigator.appName === "Netscape" &&
+        /Trident/.test(navigator.userAgent)
+      ) {
+        // IE 11
         return true;
       }
       return false;
     },
 
-    getPluginsString: function () {
-      if(this.isIE() && this.ie_activex){
+    getPluginsString: function() {
+      if (this.isIE() && this.ie_activex) {
         return this.getIEPluginsString();
       } else {
         return this.getRegularPluginsString();
       }
     },
 
-    getRegularPluginsString: function () {
-      return this.map(navigator.plugins, function (p) {
-        var mimeTypes = this.map(p, function(mt){
-          return [mt.type, mt.suffixes].join('~');
-        }).join(',');
-        return [p.name, p.description, mimeTypes].join('::');
-      }, this).join(';');
+    getRegularPluginsString: function() {
+      return this.map(
+        navigator.plugins,
+        function(p) {
+          var mimeTypes = this.map(p, function(mt) {
+            return [mt.type, mt.suffixes].join("~");
+          }).join(",");
+          return [p.name, p.description, mimeTypes].join("::");
+        },
+        this
+      ).join(";");
     },
 
-    getIEPluginsString: function () {
-      if(window.ActiveXObject){
-        var names = ['ShockwaveFlash.ShockwaveFlash',//flash plugin
-          'AcroPDF.PDF', // Adobe PDF reader 7+
-          'PDF.PdfCtrl', // Adobe PDF reader 6 and earlier, brrr
-          'QuickTime.QuickTime', // QuickTime
+    getIEPluginsString: function() {
+      if (window.ActiveXObject) {
+        var names = [
+          "ShockwaveFlash.ShockwaveFlash", //flash plugin
+          "AcroPDF.PDF", // Adobe PDF reader 7+
+          "PDF.PdfCtrl", // Adobe PDF reader 6 and earlier, brrr
+          "QuickTime.QuickTime", // QuickTime
           // 5 versions of real players
-          'rmocx.RealPlayer G2 Control',
-          'rmocx.RealPlayer G2 Control.1',
-          'RealPlayer.RealPlayer(tm) ActiveX Control (32-bit)',
-          'RealVideo.RealVideo(tm) ActiveX Control (32-bit)',
-          'RealPlayer',
-          'SWCtl.SWCtl', // ShockWave player
-          'WMPlayer.OCX', // Windows media player
-          'AgControl.AgControl', // Silverlight
-          'Skype.Detection'];
+          "rmocx.RealPlayer G2 Control",
+          "rmocx.RealPlayer G2 Control.1",
+          "RealPlayer.RealPlayer(tm) ActiveX Control (32-bit)",
+          "RealVideo.RealVideo(tm) ActiveX Control (32-bit)",
+          "RealPlayer",
+          "SWCtl.SWCtl", // ShockWave player
+          "WMPlayer.OCX", // Windows media player
+          "AgControl.AgControl", // Silverlight
+          "Skype.Detection"
+        ];
 
         // starting to detect plugins in IE
-        return this.map(names, function(name){
-          try{
+        return this.map(names, function(name) {
+          try {
             new ActiveXObject(name);
             return name;
-          } catch(e){
+          } catch (e) {
             return null;
           }
-        }).join(';');
+        }).join(";");
       } else {
         return ""; // behavior prior version 0.5.0, not breaking backwards compat.
       }
     },
 
-    getScreenResolution: function () {
+    getScreenResolution: function() {
       return [screen.height, screen.width];
     },
 
-    getCanvasFingerprint: function () {
-      var canvas = document.createElement('canvas');
-      var ctx = canvas.getContext('2d');
+    getCanvasFingerprint: function() {
+      var canvas = document.createElement("canvas");
+      var ctx = canvas.getContext("2d");
       // https://www.browserleaks.com/canvas#how-does-it-work
-      var txt = 'http://valve.github.io';
+      var txt = "http://valve.github.io";
       ctx.textBaseline = "top";
       ctx.font = "14px 'Arial'";
       ctx.textBaseline = "alphabetic";
       ctx.fillStyle = "#f60";
-      ctx.fillRect(125,1,62,20);
+      ctx.fillRect(125, 1, 62, 20);
       ctx.fillStyle = "#069";
       ctx.fillText(txt, 2, 15);
       ctx.fillStyle = "rgba(102, 204, 0, 0.7)";
@@ -262,133 +294,117 @@
   };
 
   return Fingerprint;
-
 });
 
-
-
-
 //var fp1 = new Fingerprint();
-   // var fp2 = new Fingerprint({canvas: true});
-    //var fp3 = new Fingerprint({ie_activex: true});
+// var fp2 = new Fingerprint({canvas: true});
+//var fp3 = new Fingerprint({ie_activex: true});
 
-  function setCookie(c_name, value, exdays) {
-    var exdate = new Date();
-    exdate.setDate(exdate.getDate() + exdays);
-    var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
-    document.cookie = c_name + "=" + c_value;
-	}
+function setCookie(c_name, value, exdays) {
+  var exdate = new Date();
+  exdate.setDate(exdate.getDate() + exdays);
+  var c_value =
+    escape(value) + (exdays == null ? "" : "; expires=" + exdate.toUTCString());
+  document.cookie = c_name + "=" + c_value;
+}
 
-	function setCookie_popgenie(c_name, value, exdays,domain) {
-    var exdate = new Date();
-    exdate.setDate(exdate.getDate() + exdays);
-    var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
-    document.cookie = c_name + "=" + c_value+ ";domain=.popgenie.org;path=/";;
-	}
+function setCookie_popgenie(c_name, value, exdays, domain) {
+  var exdate = new Date();
+  exdate.setDate(exdate.getDate() + exdays);
+  var c_value =
+    escape(value) + (exdays == null ? "" : "; expires=" + exdate.toUTCString());
+  document.cookie = c_name + "=" + c_value + ";domain=.popgenie.org;path=/";
+}
 
 //Very cool custom functions
 function $_GET(q, s) {
-    s = s ? s : window.location.search;
-    var re = new RegExp('&' + q + '(?:=([^&]*))?(?=&|$)', 'i');
-    return (s = s.replace('?', '&').match(re)) ? (typeof s[1] == 'undefined' ? '' : decodeURIComponent(s[1])) : undefined;
+  s = s ? s : window.location.search;
+  var re = new RegExp("&" + q + "(?:=([^&]*))?(?=&|$)", "i");
+  return (s = s.replace("?", "&").match(re))
+    ? typeof s[1] == "undefined"
+      ? ""
+      : decodeURIComponent(s[1])
+    : undefined;
 }
 
-
-
-	function getCookie(c_name) {
-		var c_value = document.cookie;
-		var c_start = c_value.indexOf(" " + c_name + "=");
-		if (c_start == -1) {
-			c_start = c_value.indexOf(c_name + "=");
-		}
-		if (c_start == -1) {
-			c_value = null;
-		} else {
-			c_start = c_value.indexOf("=", c_start) + 1;
-			var c_end = c_value.indexOf(";", c_start);
-			if (c_end == -1) {
-				c_end = c_value.length;
-			}
-			c_value = unescape(c_value.substring(c_start, c_end));
-		}
-		return c_value;
-	}
+function getCookie(c_name) {
+  var c_value = document.cookie;
+  var c_start = c_value.indexOf(" " + c_name + "=");
+  if (c_start == -1) {
+    c_start = c_value.indexOf(c_name + "=");
+  }
+  if (c_start == -1) {
+    c_value = null;
+  } else {
+    c_start = c_value.indexOf("=", c_start) + 1;
+    var c_end = c_value.indexOf(";", c_start);
+    if (c_end == -1) {
+      c_end = c_value.length;
+    }
+    c_value = unescape(c_value.substring(c_start, c_end));
+  }
+  return c_value;
+}
 
 //updategenebasket();
 
-  function pad2(number) {
-
-        return (number < 10 ? 0 : '') + number;
-    }
-
+function pad2(number) {
+  return (number < 10 ? 0 : "") + number;
+}
 
 function updategenebasket() {
+  //$("#numberofgenesSpan").illuminate();
+  $.ajax({
+    url: "plugins/genelist/crud/updatebaskets.php?id=gene",
+    type: "GET",
+    success: function(hasil) {
+      var numberc = pad2(hasil).replace(/\s/g, "");
+      //console.log(document.getElementById("numberofgenesSpan"))
+      if (document.getElementById("numberofgenesSpan") != null) {
+        document.getElementById("numberofgenesSpan").innerHTML = numberc;
+      }
 
-        //$("#numberofgenesSpan").illuminate();
-        $.ajax({
-            url: "plugins/genelist/crud/updatebaskets.php?id=gene",
-            type: "GET",
-            success: function(hasil) {
-                var numberc = pad2(hasil).replace(/\s/g, "");
-				//console.log(document.getElementById("numberofgenesSpan"))
-				if(document.getElementById("numberofgenesSpan")!=null){
+      if (
+        typeof document.getElementsByClassName("notificationcount2")
+          .innerHTML != undefined
+      ) {
+        document.getElementsByClassName(
+          "notificationcount2"
+        ).innerHTML = numberc;
+      }
 
-                document.getElementById("numberofgenesSpan").innerHTML = numberc;
-								}
-				
-                if(typeof(document.getElementsByClassName("notificationcount2").innerHTML)!=undefined){
-                document.getElementsByClassName("notificationcount2").innerHTML = numberc;
-                }
-
-                glowme("#numberofgenesSpanx");
-            }
-        });
-
-        return false;
+      glowme("#numberofgenesSpanx");
     }
+  });
 
-	    function glowme(id) {
-        //console.log("fired");
-        //		 $(id).toggleClass('active');
-        //$(id).effect('bounce',100);
-        $(id).delay(60)
-            .css({
-                opacity: 0.6
-            })
-            .animate({
-                opacity: 1
-            }, 60);
-        /*$(id).stop().animate({ opacity: 0.2,borderSpacing:"10px" }, 100, 'linear')
-                .animate({ opacity: 1,borderSpacing:"0px"  }, 100, 'linear')*/
-        //  .stop();
-        //	glower.toggleClass('active');
-        /*$(id).illuminate({
-			'intensity': '0.2',
-			'color': '#d35530',
-			'blink': 'false',
-			'blinkSpeed': '100',
-			'outerGlow': 'true',
-			'outerGlowSize': '150px',
-			'outerGlowColor': '#d35530'
-		});*/
+  return false;
+}
 
+function glowme(id) {
+  $(id)
+    .delay(60)
+    .css({
+      opacity: 0.6
+    })
+    .animate(
+      {
+        opacity: 1
+      },
+      60
+    );
+}
 
-
+function updategenebasket3() {
+  $.ajax({
+    url: "plugins/genelist/crud/updatebaskets.php?id=gene",
+    type: "GET",
+    success: function(hasil) {
+      var numberc = pad2(hasil).replace(/\s/g, "");
+      document.getElementById("numberofgenesSpan").innerHTML = numberc;
+      document.getElementById("notificationcount_2").innerHTML = numberc;
+      glowme("#numberofgenesSpanx");
     }
+  });
 
-    function updategenebasket3() {
-
-            //$("#numberofgenesSpan").illuminate();
-            $.ajax({
-                url: "plugins/genelist/crud/updatebaskets.php?id=gene",
-                type: "GET",
-                success: function(hasil) {
-                    var numberc = pad2(hasil).replace(/\s/g, "");
-                    document.getElementById("numberofgenesSpan").innerHTML = numberc;
-                    document.getElementById("notificationcount_2").innerHTML = numberc;
-                    glowme("#numberofgenesSpanx");
-                }
-            });
-
-            return false;
-        }
+  return false;
+}
