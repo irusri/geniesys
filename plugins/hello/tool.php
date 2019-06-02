261584aa6a0dd133fd45879d046b83268dbcc032
irusri@yahoo.com <irusri@yahoo.com>
2019-06-02 14:17:03 +0200

    add hello plugin

--- plugins/hello/tool.php
+++ plugins/hello/tool.php
@@ -1,3 +1,5 @@
 <?php
 echo "hello world!";
 ?>
+<br><br>This is an example plugin.<br>
+For more details please read the <a target="_blank" href="https://geniecms.readthedocs.io/en/latest/plugins/new_plugin.html">documentation</a>.
