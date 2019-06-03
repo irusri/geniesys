4c4cdaa6b98874245339b7dc581cb53fc126f033
irusri@yahoo.com <irusri@yahoo.com>
2019-06-03 08:36:23 +0200

    add home plugin for administration purpose

--- plugins/home/index.php
+++ plugins/home/index.php
@@ -10,10 +10,9 @@ for($search_num=0;$search_num<count($mennu_arr);$search_num++){
 	}
 }
 
-if(basename($_SERVER['REDIRECT_URL'])=="admin" && $_SERVER["REQUEST_URI"] =="/GenIECMS/"){
-//if(strtolower(basename(dirname(__FILE__)))== strtolower($subdir_arr[count($subdir_arr)-1]) && $menu_exist==true){
+if(strtolower(basename(dirname(__FILE__)))== strtolower($subdir_arr[count($subdir_arr)-1]) && $menu_exist==true){
 	$c['initialize_tool_plugin']=true;
-	$c['tool_plugin']="admin";//strtolower($subdir_arr[count($subdir_arr)-1]);
+	$c['tool_plugin']=strtolower($subdir_arr[count($subdir_arr)-1]);
 	
 }
 ?>
