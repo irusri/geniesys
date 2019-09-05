ab48386195607fd004d33cb29374a8344a144f7c
irusri@yahoo.com <irusri@yahoo.com>
2019-09-05 07:46:10 +0200

    update documentation

--- docs/conf.py
+++ docs/conf.py
@@ -151,7 +151,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'geniesys', u'GenIESys Documentation',
+    (master_doc, 'geniesys', u'GenIE-Sys Documentation',
      [author], 1)
 ]
 
