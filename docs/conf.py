20a68deaa5bf6e7638f340658f9ff978352e5cb5
Chanaka <irusri@yahoo.com>
2020-10-16 02:38:58 +0200

    update documentation

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.doctest','sphinx_tabs.tabs'] 
+    'sphinx.ext.doctest'] 
 from recommonmark.parser import CommonMarkParser
 
 source_parsers = {
