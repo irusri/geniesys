6b8092b0955460f2dfdc27211abe1df8e0e151bd
Chanaka <irusri@yahoo.com>
2020-10-16 02:44:23 +0200

    disable home page as its function aready integrated in the admin page

--- plugins/home/index.php
+++ plugins/home/index.php
@@ -13,7 +13,7 @@ for($search_num=0;$search_num<count($mennu_arr);$search_num++){
 $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 if(strtolower(basename(dirname(__FILE__))== strtolower($subdir_arr[count($subdir_arr)-1]) ) || $GLOBALS["base_url"] ==$actual_link ){
 //if(strtolower(basename(dirname(__FILE__))== strtolower($subdir_arr[count($subdir_arr)-1]) ) ||  $_SERVER["REQUEST_URI"] =="/geniesys/"   ){//&& $menu_exist==true 
-	$c['initialize_tool_plugin']=true;
+	$c['initialize_tool_plugin']=false;
 	$c['tool_plugin']=strtolower($subdir_arr[count($subdir_arr)-1]);
 	$c['tool_plugin']="home";
 }
